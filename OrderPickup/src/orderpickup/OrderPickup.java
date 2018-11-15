efe2c99f8ea66c50003bd131c3365937bb813e5e
yuvaraja96 <44600356+yuvaraja96@users.noreply.github.com>
2018-11-15 10:50:18 +0800

    Add files via upload

---
+++ OrderPickup/src/orderpickup/OrderPickup.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orderpickup;
+
+/**
+ *
+ * @author Yuvaraja
+ */
+import java.util.*;
+
+public class OrderPickup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        String[] custPickup1 = new String[] {"Siti","20215","0125455651"};
+        String[] custPickup2 = new String[] {"Siti","20215","0125455651"};
+        String[] custPickup3 = new String[] {"Siti","20215","0125455651"};
+          
+        String[] custDelivery1 = new String[] {"Ali","63515","251651553","Selangor"};
+        String[] custDelivery2 = new String[] {"Ali","63515","251651553","Selangor"}; 
+        String[] custDelivery3 = new String[] {"Ali","63515","251651553","Selangor"};
+        
+        
+        System.out.println("---------FIORE FLOWERSHOP SDN. BHD---------\n");
+        System.out.print("Please select type of orders: " + "\n1. Order Pickup\n" + "2. Order Delivery\n" + "Choice: ");
+        int choice = s.nextInt();
+        
+        if(choice == 1){
+                System.out.println("Hi, welcome to Order Pickup.");
+        
+                System.out.println(Arrays.toString(custPickup1) + "\n" + Arrays.toString(custPickup2)+ "\n" + Arrays.toString(custPickup3));
+        
+    }else if(choice == 2){
+                System.out.println("Hi, welcome to Order Delivery.");
+        
+                System.out.println(Arrays.toString(custDelivery1) + "\n" + Arrays.toString(custDelivery2)+ "\n" + Arrays.toString(custDelivery3));
+
+    }else{
+               System.out.println("Invalid choice. Please select suitable choice.");
+         }
+    
+    }
+}
